60cedc0daae746b19b182ccd513556633d7e453d
nathangauth88 <nathangauth@gmail.com>
2022-08-30 21:06:22 -0700

    wip: add default image & create and index

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Dependancies 
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('morgan');
 
 //Initialize the app
 
@@ -12,13 +15,64 @@ const { PORT = 4000, DATABASE_URL } = process.env;
 
 
 // Connect to MongoDb using Mongoose
+mongoose.connect(DATABASE_URL);
 
 // Mount Middleware
+app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+
+
+mongoose.connection
+.on('connected', () => console.log('Connected to MongoDB'))
+.on('error', (error) => console.log('MongoDB Error:' + error.message));
+
+// Define Models
+
+const Schema = mongoose.Schema;
+const grapplerSchema = new Schema({
+    image: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+    },
+    name: String,
+    team: String,
+    lineage: String,
+    rank: String,
+    championships: String,
+    techniques: String,
+    weight: String,
+    wins: String
+}, { timestamps: true });
+
+const Grappler = mongoose.model('Grappler', grapplerSchema);
 
 // Mount Routes
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
+
+// Routes/Controller actions
+
+// Index
+
+app.get('/grappler', async (req, res) => {
+    try {
+        res.status(200).json(await Grappler.find({}));
+    } catch (error) {
+        res.status(400).json({ message: 'bad request'})    
+    }
+});
+
+// Create 
+
+app.post('/grappler', async (req, res) => {
+    try {
+        res.status(201).json(await Grappler.create(req.body))
+    } catch (error) {
+        res.status(400).json({ message: 'bad request'});
+    }
+});
 
 // Tell the App to Listen
 app.listen(PORT, () => {
